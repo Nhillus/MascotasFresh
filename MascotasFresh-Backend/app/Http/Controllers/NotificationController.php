d53eb5eae8127901154dad40f23d6e907a9b3d67
Nhillus <xabier762@gmail.com>
2021-09-25 11:35:29 -0500

    Hechos algunos cambios para mejor experiencia usuario con la vision que tengo sobre este sistema se muestra ahora el mensaje de notificacion en este momento con baja de producto el nombre del producto y su cantidad

--- MascotasFresh-Backend/app/Http/Controllers/NotificationController.php
+++ MascotasFresh-Backend/app/Http/Controllers/NotificationController.php
@@ -5,13 +5,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Notification;
-
+use App\Producto;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class NotificationController extends Controller
 {
     //
     public function index() {
-        $allNotificactions = Notification::all();
-        return ['notifications'=> $allNotificactions];
+        $allNotifications = Notification::all();
+        if($allNotifications)
+        {
+            foreach($allNotifications as $Notification)
+            {
+                try {
+                    $ProductoNombre = Producto::where('id',$Notification->product_id)->pluck('nombre');
+                    $ProductoCantidad = Producto::where('id',$Notification->product_id)->pluck('cantidad');
+                    $Notification['ProductoNombre'] = $ProductoNombre;
+                    $Notification['ProductoCantidad'] = $ProductoCantidad;
+                } catch (ModelNotFoundException $e) {
+                    //throw $th;
+                    return response()->json([
+                        'message' => 'Product not found.',
+                        $e
+                    ], 404);
+                }
+            }
+        }
+        return ['notifications'=> $allNotifications];
     }
 }
